3c37594397aca06d30ca9b5502a855f62085204b
VasylPopaz <pafos1997@gmail.com>
2024-02-28 10:32:24 +0100

    upd index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/movies-routing-react">
       <App />
       <GlobalStyle />
     </BrowserRouter>
